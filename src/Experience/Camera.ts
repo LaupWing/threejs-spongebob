904a339d3c509a51137e03219e6ac47d507760e7
LaupWing <loc_strict@hotmail.com>
2023-02-10 14:26:51 +0100

    udpate

--- src/Experience/Camera.ts
+++ src/Experience/Camera.ts
@@ -1,3 +1,30 @@
+import Experience from "./Experience"
+import Sizes from "./utils/Sizes"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
 export default class Camera {
-   
+   private experience: Experience
+   private sizes: Sizes
+   private scene: THREE.Scene
+   private canvas: HTMLCanvasElement
+   private controls: OrbitControls
+   public instance: THREE.PerspectiveCamera
+
+   constructor(experience: Experience){
+      this.experience = experience
+      this.sizes = this.experience.sizes
+      this.scene = this.experience.scene
+      this.canvas = this.experience.canvas
+      this.instance = new THREE.PerspectiveCamera(
+         75,
+         this.sizes.width / this.sizes.height,
+         0.1,
+         100
+      )
+      this.instance.position.set(0.25, -0.25, 1)
+      this.scene.add(this.instance)
+      this.controls = new OrbitControls(this.instance, this.canvas)
+      this.controls.enableDamping = true
+   }
 }
