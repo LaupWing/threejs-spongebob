94843be675e829c3562f5a607763e575f488cbc4
LaupWing <loc_strict@hotmail.com>
2023-02-10 14:13:10 +0100

    added vetnemititer

--- src/Experience/utils/Sizes.ts
+++ src/Experience/utils/Sizes.ts
@@ -1,9 +1,12 @@
-export default class Sizes {
+import { EventEmitter } from "./EventEmitter"
+
+export default class Sizes extends EventEmitter {
    public width: number
    public height: number
    public pixelRatio: number
 
    constructor() {
+      super()
       this.width = window.innerWidth
       this.height = window.innerHeight
       this.pixelRatio = Math.min(window.devicePixelRatio, 2)
