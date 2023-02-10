92c841e4c665720f00c6f854b38a4d0025d6962f
LaupWing <loc_strict@hotmail.com>
2023-02-10 14:18:48 +0100

    udpat

---
+++ src/Experience/utils/Time.ts
@@ -0,0 +1,30 @@
+import * as THREE from "three"
+import { EventEmitter } from "./EventEmitter"
+
+export default class Time extends EventEmitter {
+   private start: number
+   private current: number
+   public elapsed: number
+   public delta: number
+   public clock: THREE.Clock
+
+   constructor(){
+      super()
+      this.start = Date.now()
+      this.current = this.start
+      this.elapsed = 0
+      this.delta = 0
+      this.clock = new THREE.Clock()
+
+      window.requestAnimationFrame(() => {
+         this.tick()
+      })
+   }
+
+   tick(){
+      const currentTime = Date.now()
+      this.delta = currentTime - this.current
+      this.current = currentTime
+      this.elapsed = this.current - this.start
+   }
+}
