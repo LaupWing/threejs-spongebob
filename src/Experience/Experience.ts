85a4eb813ab961016af122750c7fb600b75772b2
LaupWing <loc_strict@hotmail.com>
2023-02-10 15:28:54 +0100

    udpate

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,15 +1,18 @@
 import * as THREE from "three"
+import Camera from "./Camera"
 import Sizes from "./utils/Sizes"
 
 export default class Experience {
    public canvas: HTMLCanvasElement
    public scene: THREE.Scene
    public sizes: Sizes
+   public camera: Camera
 
    constructor(canvas: HTMLCanvasElement) {
       this.canvas = canvas
       this.sizes = new Sizes()
       this.scene = new THREE.Scene()
+      this.camera = new Camera(this)
 
       this.sizes.on("resize", () => {
          this.resize()
@@ -17,6 +20,6 @@ export default class Experience {
    }
 
    resize() {
-      
+
    }
 }
