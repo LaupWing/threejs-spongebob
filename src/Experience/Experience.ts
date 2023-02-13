4f4a71edc9e155eb2cccc4a0f37cf30d0e5d6ed1
LaupWing <loc_strict@hotmail.com>
2023-02-13 13:58:43 +0100

    udpate and resize

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,25 +1,36 @@
 import * as THREE from "three"
 import Camera from "./Camera"
 import Sizes from "./utils/Sizes"
+import Time from "./utils/Time"
 
 export default class Experience {
    public canvas: HTMLCanvasElement
    public scene: THREE.Scene
    public sizes: Sizes
    public camera: Camera
+   public time: Time
 
    constructor(canvas: HTMLCanvasElement) {
       this.canvas = canvas
       this.sizes = new Sizes()
+      this.time = new Time()
       this.scene = new THREE.Scene()
       this.camera = new Camera(this)
 
       this.sizes.on("resize", () => {
          this.resize()
       })
+
+      this.time.on("tick", () => {
+         this.update()
+      })
    }
 
    resize() {
+      this.camera.update()
+   }
+
+   update() {
 
    }
 }
--- src/Experience/Renderer.ts
+++ src/Experience/Renderer.ts
@@ -1,21 +1,37 @@
 import Experience from "./Experience"
 import * as THREE from "three"
+import Sizes from "./utils/Sizes"
+import Camera from "./Camera"
 
 export default class Renderer {
    private experience: Experience
    private instance: THREE.WebGLRenderer
+   private scene: THREE.Scene
    private canvas: HTMLCanvasElement
-
+   private sizes: Sizes
+   private camera: Camera
+   
    constructor(experience: Experience){
       this.experience = experience
       this.canvas = this.experience.canvas
+      this.sizes = this.experience.sizes
+      this.scene = this.experience.scene
+      this.camera = this.experience.camera
       this.instance = new THREE.WebGLRenderer({
          canvas: this.canvas,
          antialias: true
       })
+      this.instance.setClearColor("black")
+      this.instance.setSize(this.sizes.width, this.sizes.height)
+      this.instance.setPixelRatio(Math.min(window.devicePixelRatio, 2))
    }
 
    resize() {
-      this.instance.setSize
+      this.instance.setSize(this.sizes.width, this.sizes.height)
+      this.instance.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   }
+
+   update(){
+      this.instance.render(this.scene, this.camera.instance)
    }
 }
