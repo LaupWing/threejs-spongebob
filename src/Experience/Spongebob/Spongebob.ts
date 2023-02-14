030a9565c58071f421a43ec2e31300220a99f667
LaupWing <loc_strict@hotmail.com>
2023-02-14 15:01:45 +0100

    udpate

--- src/Experience/Spongebob/Spongebob.ts
+++ src/Experience/Spongebob/Spongebob.ts
@@ -1,20 +1,14 @@
 import Experience from "../Experience"
 import * as THREE from "three"
+import SpongebobBody from "./SpongebobBody"
 
 export default class Spongebob{
    private scene: Experience["scene"]
-   private geometry: THREE.BoxGeometry
-   private material: THREE.MeshStandardMaterial
-   private cube: THREE.Mesh
+   private body
 
    constructor(experience: Experience){
       this.scene = experience.scene
-      this.geometry = new THREE.BoxGeometry(0.8, 0.8, 0.4)
-      this.material = new THREE.MeshStandardMaterial({
-         color: 0xfff463
-      }) 
-      this.cube = new THREE.Mesh(this.geometry, this.material) 
-      this.cube.position.y = 0.2
+      
       this.scene.add(this.cube)
    }
 }
--- src/Experience/Spongebob/SpongebobBody.ts
+++ src/Experience/Spongebob/SpongebobBody.ts
@@ -1,5 +1,16 @@
+import * as THREE from "three"
+
 export default class SpongebobBody {
+   private geometry: THREE.BoxGeometry
+   private material: THREE.MeshStandardMaterial
+   private instance: THREE.Mesh
+
    constructor(){
-      
+      this.geometry = new THREE.BoxGeometry(0.8, 0.8, 0.4)
+      this.material = new THREE.MeshStandardMaterial({
+         color: 0xfff463
+      }) 
+      this.instance = new THREE.Mesh(this.geometry, this.material) 
+      this.instance.position.y = 0.2
    }
 }
