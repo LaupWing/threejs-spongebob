6e734ae36bed2ee6c0730f225e0296404d633a0c
LaupWing <loc_strict@hotmail.com>
2023-02-14 13:58:28 +0100

    LIghts

--- src/Experience/Spongebob/Spongebob.ts
+++ src/Experience/Spongebob/Spongebob.ts
@@ -4,13 +4,13 @@ import * as THREE from "three"
 export default class Spongebob{
    private scene: Experience["scene"]
    private geometry: THREE.BoxGeometry
-   private material: THREE.MeshBasicMaterial
+   private material: THREE.MeshStandardMaterial
    private cube: THREE.Mesh
 
    constructor(experience: Experience){
       this.scene = experience.scene
       this.geometry = new THREE.BoxGeometry(1, 1, 1)
-      this.material = new THREE.MeshBasicMaterial({
+      this.material = new THREE.MeshStandardMaterial({
          color: 0x00ff00
       }) 
       this.cube = new THREE.Mesh(this.geometry, this.material) 
