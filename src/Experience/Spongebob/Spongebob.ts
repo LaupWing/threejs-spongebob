63510c5891d1f3f47b291856697849348850d80f
LaupWing <loc_strict@hotmail.com>
2023-02-15 13:52:35 +0100

    udpate

--- src/Experience/Spongebob/Spongebob.ts
+++ src/Experience/Spongebob/Spongebob.ts
@@ -1,17 +1,22 @@
 import Experience from "../Experience"
+import SpongebobBody from "./SpongebobBody"
 import SpongebobHead from "./SpongebobHead"
 import SpongebobPants from "./SpongebobPants"
 
 export default class Spongebob{
    private scene: Experience["scene"]
-   private body: SpongebobHead
+   private head: SpongebobHead
    private pants: SpongebobPants
+   private body: SpongebobBody
 
    constructor(experience: Experience){
       this.scene = experience.scene
-      this.body = new SpongebobHead()
+      this.head = new SpongebobHead()
+      this.body = new SpongebobBody()
       this.pants = new SpongebobPants()
+
+      this.scene.add(this.head.instance)
       this.scene.add(this.body.instance)
-      this.scene.add(this.pants.instance)
+      // this.scene.add(this.pants.instance)
    }
 }
---
+++ src/Experience/Spongebob/SpongebobBody.ts
@@ -0,0 +1,16 @@
+import * as THREE from "three"
+
+export default class SpongebobBody {
+   private geometry: THREE.BoxGeometry
+   private material: THREE.MeshStandardMaterial
+   public instance: THREE.Mesh
+
+   constructor(){
+      this.geometry = new THREE.BoxGeometry(0.8, 0.3, 0.4)
+      this.material = new THREE.MeshStandardMaterial({
+         color: 0xfbfff7
+      }) 
+      this.instance = new THREE.Mesh(this.geometry, this.material) 
+      this.instance.position.y = 0
+   }
+}
