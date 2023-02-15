11ab66411440718093eebcac67622e3985ec7caf
LaupWing <loc_strict@hotmail.com>
2023-02-15 13:49:34 +0100

    chabnged naming to head

--- src/Experience/Spongebob/Spongebob.ts
+++ src/Experience/Spongebob/Spongebob.ts
@@ -1,15 +1,15 @@
 import Experience from "../Experience"
-import SpongebobBody from "./SpongebobBody"
+import SpongebobHead from "./SpongebobHead"
 import SpongebobPants from "./SpongebobPants"
 
 export default class Spongebob{
    private scene: Experience["scene"]
-   private body: SpongebobBody
+   private body: SpongebobHead
    private pants: SpongebobPants
 
    constructor(experience: Experience){
       this.scene = experience.scene
-      this.body = new SpongebobBody()
+      this.body = new SpongebobHead()
       this.pants = new SpongebobPants()
       this.scene.add(this.body.instance)
       this.scene.add(this.pants.instance)
--- src/Experience/Spongebob/SpongebobHead.ts
+++ src/Experience/Spongebob/SpongebobHead.ts
@@ -1,16 +1,16 @@
 import * as THREE from "three"
 
-export default class SpongebobBody {
+export default class SpongebobHead {
    private geometry: THREE.BoxGeometry
    private material: THREE.MeshStandardMaterial
    public instance: THREE.Mesh
 
    constructor(){
-      this.geometry = new THREE.BoxGeometry(0.8, 0.8, 0.4)
+      this.geometry = new THREE.BoxGeometry(0.8, 0.7, 0.4)
       this.material = new THREE.MeshStandardMaterial({
          color: 0xfff463
       }) 
       this.instance = new THREE.Mesh(this.geometry, this.material) 
-      this.instance.position.y = 0.55
+      this.instance.position.y = 0.5
    }
 }
