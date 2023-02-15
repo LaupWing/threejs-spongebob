278dfc83d1ce10a197dca73d71868f1497e4f3bc
LaupWing <loc_strict@hotmail.com>
2023-02-15 14:09:53 +0100

    update

--- src/Experience/Spongebob/Spongebob.ts
+++ src/Experience/Spongebob/Spongebob.ts
@@ -2,6 +2,7 @@ import Experience from "../Experience"
 import SpongebobBody from "./SpongebobBody"
 import SpongebobHead from "./SpongebobHead"
 import SpongebobLegsLeft from "./SpongebobLegs/SpongebobLegsLeft/SpongebobLegsLeft"
+import SpongebobLegsRight from "./SpongebobLegs/SpongebobLegsRight/SpongebobLegsRight"
 import SpongebobPants from "./SpongebobPants"
 
 export default class Spongebob{
@@ -10,6 +11,7 @@ export default class Spongebob{
    private pants: SpongebobPants
    private body: SpongebobBody
    private legs: SpongebobLegsLeft
+   private legs_r: SpongebobLegsRight
 
    constructor(experience: Experience){
       this.scene = experience.scene
@@ -17,7 +19,9 @@ export default class Spongebob{
       this.body = new SpongebobBody()
       this.pants = new SpongebobPants()
       this.legs = new SpongebobLegsLeft()
+      this.legs_r = new SpongebobLegsRight()
 
+      this.scene.add(this.legs_r.instance)
       this.scene.add(this.legs.instance)
       this.scene.add(this.head.instance)
       this.scene.add(this.body.instance)
