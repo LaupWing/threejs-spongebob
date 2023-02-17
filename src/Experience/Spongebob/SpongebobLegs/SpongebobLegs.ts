cf19c252c361f50de1427950896867e0a66e238e
LaupWing <loc_strict@hotmail.com>
2023-02-17 14:07:37 +0100

    udpate

--- src/Experience/Spongebob/SpongebobLegs/SpongebobLegs.ts
+++ src/Experience/Spongebob/SpongebobLegs/SpongebobLegs.ts
@@ -2,17 +2,19 @@ import * as THREE from "three"
 
 export default class SpongebobLegsLegs {
    private segmentCount: number
+   private width: number
    private geometry: THREE.CylinderGeometry
+   private instance: THREE.Group
    private material: THREE.MeshStandardMaterial
-   public instance: THREE.Mesh
 
    constructor(){
       this.segmentCount = 6
+      this.width = 0.1
       this.geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.1, 8)
       this.material = new THREE.MeshStandardMaterial({
          color: 0xc26c31
       })
-      this.instance = new THREE.Mesh(this.geometry, this.material) 
+      this.instance = new THREE.Group()
       this.instance.position.y = -0.18
       this.instance.position.x = 0.2
    }
