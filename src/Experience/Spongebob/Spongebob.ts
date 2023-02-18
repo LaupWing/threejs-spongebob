1e46e925dacaf804efdeb2f4756511c580cf429a
LaupWing <loc_strict@hotmail.com>
2023-02-18 14:01:29 +0100

    udpate

--- src/Experience/Spongebob/Spongebob.ts
+++ src/Experience/Spongebob/Spongebob.ts
@@ -1,6 +1,7 @@
 import Experience from "../Experience"
 import SpongebobBody from "./SpongebobBody"
 import SpongebobHead from "./SpongebobHead"
+import SpongebobLegs from "./SpongebobLegs/SpongebobLegs"
 import SpongebobPants from "./SpongebobPants"
 
 export default class Spongebob{
@@ -8,15 +9,18 @@ export default class Spongebob{
    private head: SpongebobHead
    private pants: SpongebobPants
    private body: SpongebobBody
+   private legs: SpongebobLegs
 
    constructor(experience: Experience){
       this.scene = experience.scene
       this.head = new SpongebobHead()
       this.body = new SpongebobBody()
       this.pants = new SpongebobPants()
+      this.legs = new SpongebobLegs()
 
       this.scene.add(this.head.instance)
       this.scene.add(this.body.instance)
       this.scene.add(this.pants.instance)
+      this.scene.add(this.legs.instance)
    }
 }
--- src/Experience/Spongebob/SpongebobLegs/SpongebobLegs.ts
+++ src/Experience/Spongebob/SpongebobLegs/SpongebobLegs.ts
@@ -1,9 +1,17 @@
 import * as THREE from "three"
+import SpongebobLegsUpper from "./SpongebobLegsUpper"
 
-export default class SpongebobLegsLegs {
+export default class SpongebobLegs {
    public instance: THREE.Group
+   private upper_l: SpongebobLegsUpper
+   private upper_r: SpongebobLegsUpper
 
    constructor(){
       this.instance = new THREE.Group()
+      this.upper_l = new SpongebobLegsUpper(0.2)
+      this.upper_r = new SpongebobLegsUpper(-0.2)
+      
+      this.instance.add(this.upper_l.instance)
+      this.instance.add(this.upper_r.instance)
    }
 }
