daacf14587cfab357582be2062c631fbd8673f40
LaupWing <loc_strict@hotmail.com>
2023-02-18 14:33:30 +0100

    udpate

--- src/Experience/Spongebob/SpongebobLegs/SpongebobLegsSock.ts
+++ src/Experience/Spongebob/SpongebobLegs/SpongebobLegsSock.ts
@@ -2,6 +2,7 @@ import * as THREE from "three"
 
 export default class SpongebobLegsSock {
    private base: THREE.Mesh
+   private blueStripe: THREE.Mesh
    public instance: THREE.Group
 
    constructor(xPostition:number){  
@@ -11,8 +12,17 @@ export default class SpongebobLegsSock {
             color: 0xfbfff7
          })
       ) 
+      this.blueStripe = new THREE.Mesh(
+         new THREE.CylinderGeometry(0.041, 0.041, 0.02, 6, 1, true), 
+         new THREE.MeshStandardMaterial({
+            color: 0x0e4da8
+         })
+      ) 
+      this.blueStripe.position.y = 0.05
+      
       this.instance = new THREE.Group() 
       this.instance.add(this.base)
+      this.instance.add(this.blueStripe)
       this.instance.position.y = -0.41
       this.instance.position.x = xPostition
    }
