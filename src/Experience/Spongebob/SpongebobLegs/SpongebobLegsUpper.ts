1aadfe5499a0afa4bccb46af89a8a3be52067d3a
LaupWing <loc_strict@hotmail.com>
2023-02-18 13:57:45 +0100

    cleann up

--- src/Experience/Spongebob/SpongebobLegs/SpongebobLegsUpper.ts
+++ src/Experience/Spongebob/SpongebobLegs/SpongebobLegsUpper.ts
@@ -5,13 +5,13 @@ export default class SpongebobLegsUpper {
    private material: THREE.MeshStandardMaterial
    public instance: THREE.Mesh
 
-   constructor(){
+   constructor(xPostition:number){
       this.geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.1, 6)
       this.material = new THREE.MeshStandardMaterial({
          color: 0xc26c31
       })
       this.instance = new THREE.Mesh(this.geometry, this.material) 
       this.instance.position.y = -0.18
-      this.instance.position.x = 0.2
+      this.instance.position.x = xPostition
    }
 }
