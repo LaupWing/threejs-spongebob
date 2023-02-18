0fd71e390d75503304dd3b8709682e300bb771ee
LaupWing <loc_strict@hotmail.com>
2023-02-18 14:29:25 +0100

    update

--- src/Experience/Spongebob/SpongebobLegs/SpongebobLegs.ts
+++ src/Experience/Spongebob/SpongebobLegs/SpongebobLegs.ts
@@ -7,7 +7,7 @@ export default class SpongebobLegs {
    public instance: THREE.Group
    private upper_l: SpongebobLegsUpper
    private middle_l: SpongebobLegsMiddle
-   // private sock_l: SpongebobLegsSock
+   private sock_l: SpongebobLegsSock
 
    private upper_r: SpongebobLegsUpper
    private middle_r: SpongebobLegsMiddle
@@ -17,14 +17,14 @@ export default class SpongebobLegs {
 
       this.upper_l = new SpongebobLegsUpper(0.2)
       this.middle_l = new SpongebobLegsMiddle(0.2)
-      // this.sock_l = new SpongebobLegsSock(0.2)
+      this.sock_l = new SpongebobLegsSock(0.2)
       
       this.upper_r = new SpongebobLegsUpper(-0.2)
       this.middle_r = new SpongebobLegsMiddle(-0.2)
 
       this.instance.add(this.upper_l.instance)
       this.instance.add(this.middle_l.instance)
-      // this.instance.add(this.sock_l.instance)
+      this.instance.add(this.sock_l.instance)
 
       this.instance.add(this.upper_r.instance)
       this.instance.add(this.middle_r.instance)
--- src/Experience/Spongebob/SpongebobLegs/SpongebobLegsSock.ts
+++ src/Experience/Spongebob/SpongebobLegs/SpongebobLegsSock.ts
@@ -1,17 +1,19 @@
 import * as THREE from "three"
 
 export default class SpongebobLegsSock {
-   private geometry: THREE.CylinderGeometry
-   private material: THREE.MeshStandardMaterial
-   public instance: THREE.Mesh
+   private base: THREE.Mesh
+   public instance: THREE.Group
 
-   constructor(xPostition:number){
-      this.geometry = new THREE.CylinderGeometry(0.04, 0.04, 0.03, 6)
-      this.material = new THREE.MeshStandardMaterial({
-         color: 0xfbfff7
-      })
-      this.instance = new THREE.Mesh(this.geometry, this.material) 
-      this.instance.position.y = -0.345
+   constructor(xPostition:number){  
+      this.base = new THREE.Mesh(
+         new THREE.CylinderGeometry(0.04, 0.04, 0.16, 6), 
+         new THREE.MeshStandardMaterial({
+            color: 0xfbfff7
+         })
+      ) 
+      this.instance = new THREE.Group() 
+      this.instance.add(this.base)
+      this.instance.position.y = -0.41
       this.instance.position.x = xPostition
    }
 }
