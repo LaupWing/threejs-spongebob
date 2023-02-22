58a9f1f22a9d8db2e19aca116e34c43079061f99
LaupWing <loc_strict@hotmail.com>
2023-02-22 15:19:49 +0100

    update

--- src/Experience/Spongebob/SpongebobArms/SpongebobArms.ts
+++ src/Experience/Spongebob/SpongebobArms/SpongebobArms.ts
@@ -7,6 +7,7 @@ export default class SpongebobArms {
       this.instance = new THREE.Group()
       this.instance.add(
          this.createUpper(0.45),
+         this.createLower(0.45),
          this.createUpper(-0.45),
       )
    }
@@ -24,7 +25,24 @@ export default class SpongebobArms {
       mesh.position.y = 0.25
       mesh.position.x = offset
       mesh.position.z = 0
-      // mesh.scale.z = 0.5
+      mesh.scale.y = 1.1
+
+      return mesh
+   }
+
+   createLower(offset: number){
+      const geometry = new THREE.CylinderGeometry(
+         0.03, 0.03, 0.2, 8
+      )
+      const material = new THREE.MeshStandardMaterial({
+         color: 0xfff463,
+         side: THREE.DoubleSide
+      }) 
+      const mesh = new THREE.Mesh(geometry, material) 
+      
+      mesh.position.y = 0.05
+      mesh.position.x = offset
+      mesh.position.z = 0
       mesh.scale.y = 1.1
 
       return mesh
